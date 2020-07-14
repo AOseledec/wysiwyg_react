d063d1b9b8189a079df0a5847c54f34957513f43
aoseledec <artemij.oseledets@yandex.ru>
2020-07-14 14:35:54 +0800

    Updates

---
+++ precache-manifest.46d3e13427a0a00de7d060a36f0bdb9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6a607b42e3fb72126cfeba2d3ca02f0",
+    "url": "/wysiwyg_react/index.html"
+  },
+  {
+    "revision": "b63ec2e35cb4012430ec",
+    "url": "/wysiwyg_react/static/css/2.260df227.chunk.css"
+  },
+  {
+    "revision": "8e33e17f1cfa5dc02cb6",
+    "url": "/wysiwyg_react/static/css/main.315a9ad6.chunk.css"
+  },
+  {
+    "revision": "b63ec2e35cb4012430ec",
+    "url": "/wysiwyg_react/static/js/2.82e94d41.chunk.js"
+  },
+  {
+    "revision": "1ce0cd407133c7d2605caff662305748",
+    "url": "/wysiwyg_react/static/js/2.82e94d41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e33e17f1cfa5dc02cb6",
+    "url": "/wysiwyg_react/static/js/main.bc2bfb78.chunk.js"
+  },
+  {
+    "revision": "e45f65dec4a6d954495a",
+    "url": "/wysiwyg_react/static/js/runtime-main.bb620cd8.js"
+  }
+]);
